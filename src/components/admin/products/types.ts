991e536ad8e3bf2eb43fd11439bb68b6612459a9
Krouver <storage.git@gmail.com>
2023-05-25 21:06:07 +0300

    HW11

---
+++ src/components/admin/products/types.ts
@@ -0,0 +1,13 @@
+export interface IProductCreate {
+  name: string;
+  priority: number;
+  categoryId: number;
+  price: number;
+  description: string;
+  ids: number[];
+}
+
+export interface ICategorySelect {
+  id: number,
+  title: string
+}
